3ced4d30fd29ac0f3a40bd2b4692009b81b454ee
TimzOwen <ktimothy@kabarak.ac.ke>
2020-06-30 23:40:52 +0300

    firebase registration

--- app/src/main/java/com/owen/firebasegads/SignUpActivity.java
+++ app/src/main/java/com/owen/firebasegads/SignUpActivity.java
@@ -1,5 +1,6 @@
 package com.owen.firebasegads;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,15 +8,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SignUpActivity extends AppCompatActivity {
 
     private EditText etPassword,etEmail;
     private Button btnSignUp;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        //initialize firebase authentication
+        mAuth = FirebaseAuth.getInstance();
 
         //Ids method
         fetchIds();
@@ -24,6 +34,7 @@ public class SignUpActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //Register to firebase.
+                userRegistration();
             }
         });
 
@@ -32,8 +43,35 @@ public class SignUpActivity extends AppCompatActivity {
     //create method to collect IDs
     public void fetchIds()
     {
+        // TODO  Collect ids
         etEmail = findViewById(R.id.etEmail);
         etPassword = findViewById(R.id.etPass);
         btnSignUp = findViewById(R.id.btnCreate);
+    }
+
+    //create a method to validate user registration to the database
+    public void userRegistration()
+    {
+        //collect the Email and password inputs
+        String email = etEmail.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
+
+        //now push the data to the database by signing up new users
+        mAuth.createUserWithEmailAndPassword(email,password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        if (task.isSuccessful())
+                        {
+                            //put data to db
+                        }
+                        else
+                        {
+                            //toast try again
+                        }
+                    }
+                });
+        
     }
 }
